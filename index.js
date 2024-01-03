f081be74d5daf545537ada396ec57d7bdefafadf
Dj <82107630+CandidCins@users.noreply.github.com>
2024-01-03 17:47:55 -0500

    add whole new design without external HTML or CSS

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// mobile menu
+function toggleMenu() {
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
 
-//# <- ID of element
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
+}
+
+document.querySelectorAll('.menu-links a').forEach(link => {
+  link.addEventListener('click', toggleMenu);
+});
+
+document.querySelector('.hamburger-icon').addEventListener('click', toggleMenu);
+
 document.addEventListener('DOMContentLoaded', function() {
-  let burger = document.getElementById('burger');
-  let navLinks = document.getElementById('nav-links');
+  document.querySelector('.btn.btn-color-2').addEventListener('click', () => {
+      window.open('./assets/My_RESUME.pdf');
+  });
 
-  burger.addEventListener("click", (event) => {
-    navLinks.classList.toggle("is-active");
-    event.preventDefault();
+  document.querySelector('.btn.btn-color-1').addEventListener('click', () => {
+      location.href = './#contact';
+  });
+
+  const socialIcons = document.querySelectorAll('#profile .icon');
+  const socialLinks = ['https://linkedin.com/in/dj-addison-69826b1b6', 'https://github.com/CandidCins'];
+  socialIcons.forEach((icon, index) => {
+      icon.addEventListener('click', () => {
+          location.href = socialLinks[index];
+      });
+  });
+
+  document.querySelector('.project-btn').addEventListener('click', () => {
+      location.href = 'https://github.com/CandidCins';
   });
 });
